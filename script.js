428fa366535e59c937fc233043dbecfba768c7fc
Shibinplathara <120809133+Shibinplathara@users.noreply.github.com>
2022-12-17 22:25:27 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('a')
+var pass = document.getElementById('pass')
+var lab = document.getElementById("label")
+
+
+function cheak(){
+	if (pass.value ==="rtx360-malavika") {
+		 console.log("Login Successfull")
+		 document.location.href="Malavika.html"
+	}
+}
